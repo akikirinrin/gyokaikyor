6351e3393ff1ff38c0190af19f25d4d078766f9a
Akihiro Manabe <50477364+akimanabe@users.noreply.github.com>
2020-04-17 12:39:45 +0900

    :rotating_light: Fix lint warnings

--- R/test_utils.R
+++ R/test_utils.R
@@ -1,12 +1,13 @@
-prefec_catch_sum_test <- function(dat, prefec, location, fishery, expected_data) {
-  our_data <-
-    dat %>%
-    dplyr::filter(Prefec == prefec &
-                  Location == location &
-                  Fishery == fishery
-    ) %>%
-    dplyr::pull(Catch_ton) %>%
-    sum()
+prefec_catch_sum_test <-
+  function(dat, prefec, location, fishery, expected_data) {
+    our_data <-
+      dat %>%
+      dplyr::filter(Prefec == prefec &
+                      Location == location &
+                      Fishery == fishery
+      ) %>%
+      dplyr::pull(Catch_ton) %>%
+      sum()
 
-  expect_equal(our_data, expected_data)
-}
+    expect_equal(our_data, expected_data)
+  }
