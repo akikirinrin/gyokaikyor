0148e72272479518c35140a8701797177c9734d4
Akira Hayashi <smxshxishxad@me.com>
2020-04-28 17:29:52 +0900

    :white_check_mark: Expect warning

--- tests/testthat/test-parse_legacy.R
+++ tests/testthat/test-parse_legacy.R
@@ -1,7 +1,10 @@
 context("Function parse_legacy()")
 
 test_that("", {
-  expect_is(parse_legacy.maiwashi_maaji("excel/gyokaikyo_dummy_maaji.xlsx",
-                                        species = "マアジ", year = 2019),
-            "data.frame")
+  expect_warning(
+    expect_is(parse_legacy.maiwashi_maaji("excel/gyokaikyo_dummy_maaji.xlsx",
+                                          species = "マアジ", year = 2019),
+              "data.frame"),
+    "NAs introduced by coercion"
+  )
 })
