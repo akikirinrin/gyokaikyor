f51107585cd47b4ec1fb54c477bba73819b329f3
Akira Hayashi <smxshxishxad@me.com>
2020-05-15 16:38:52 +0900

    :building_construction: Make UI function for parsing legacy Excel
    Resolve #52

--- R/parse_legacy.R
+++ R/parse_legacy.R
@@ -1,29 +1,27 @@
+legacy <- function(fname, year, spcs) {
+  structure(list(fname = fname, year = year, spcs = spcs), class = spcs)
+}
+
 #' Parse pseudo database of gyokaikyo project into data frame
 #'
 #' Note that this function is for maiwashi and maaji
 #'
 #' @inheritParams parse_legacy.sabarui
 #' @param fname Name of legacy catch database file of maiwashi and maaji
-#' @export
-parse_legacy.maiwashi_maaji <- function(fname, year, species) {
-  spcs_expected <- c("マイワシ", "マアジ")
-  if (species %not_in% spcs_expected) {
-    stop(paste("Spcs name must be one of:",
-               paste0(spcs_expected, collapse = ", ")))
-  }
-  lucifer::rebel(fname, sheet_regex = generate_prefec(),
-                 cluster = list(dir = "v",
-                                pos = 2,
-                                regex = "\\u6708\\uff3c\\u5e74", #月＼年
-                                offset = c(0, 0),
-                                ends = list(row = "12",
-                                            col = as.character(year)),
-                                info = list(value_offset = c(1, -1),
-                                            value_dim = c(1, 1))),
-                 crop = list(direction = "v",
-                             pos = 1,
-                             regex = "以下入力データシート",
-                             use_after = FALSE)) %>%
+parse_legacy <- function(x) {
+    lucifer::rebel(x$fname, sheet_regex = generate_prefec(),
+                   cluster = list(dir = "v",
+                                  pos = 2,
+                                  regex = "\\u6708\\uff3c\\u5e74", #月＼年
+                                  offset = c(0, 0),
+                                  ends = list(row = "12",
+                                              col = as.character(x$year)),
+                                  info = list(value_offset = c(1, -1),
+                                              value_dim = c(1, 1))),
+                   crop = list(direction = "v",
+                               pos = 1,
+                               regex = "以下入力データシート",
+                               use_after = FALSE)) %>%
     dplyr::rename(
       month = stringi::stri_unescape_unicode("\\u6708\\\\\\u5e74"), #月\年
       fishery = key1
@@ -32,15 +30,26 @@ parse_legacy.maiwashi_maaji <- function(fname, year, species) {
                   dplyr::matches("\\d{4}")) %>%
     tidyr::gather(-month, -fishery, -fname, -sheet,
                   key = "year", value = "catch_ton") %>%
-    dplyr::mutate(species = species,
+    dplyr::mutate(species = x$spcs,
                   year = as.integer(year),
                   month = as.integer(month),
-                  prefec = sheet,
-                  catch_ton = dplyr::if_else(species == "maaji" &
-                                               prefec == "静岡",
-                                             as.double(catch_ton) / 1000,
-                                             as.double(catch_ton))) %>%
+                  prefec = sheet) %>%
     dplyr::filter(!is.na(fishery) &
                     !is.na(year)) %>%
     finalize_df()
+}
+
+parse <- function(x) {
+  UseMethod("parse")
+}
+parse.maaji <- function(x) {
+  parse_legacy(x) %>%
+    dplyr::mutate(Catch_ton = dplyr::if_else(Prefec == "静岡",
+                                             as.double(Catch_ton) / 1000,
+                                             as.double(Catch_ton)))
+}
+
+getdata <- function(fname, year, spcs) {
+  legacy(fname, year = year, spcs = spcs) %>%
+    parse()
 }
--- tests/testthat/test-parse_legacy.R
+++ tests/testthat/test-parse_legacy.R
@@ -2,8 +2,9 @@ context("Function parse_legacy()")
 
 test_that("", {
   expect_na_coercion(
-    expect_is(parse_legacy.maiwashi_maaji("excel/gyokaikyo_dummy_maaji.xlsx",
-                                          species = "マアジ", year = 2019),
+    expect_is(parse_legacy(list(fname = "excel/gyokaikyo_dummy_maaji.xlsx",
+                                spcs = "maaji",
+                                year = 2019)),
               "data.frame")
   )
 })
