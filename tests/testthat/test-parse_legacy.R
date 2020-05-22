47e68ed9e211d7ba191944812a6c135e56bab000
Akira Hayashi <smxshxishxad@me.com>
2020-05-22 13:56:47 +0900

    :truck: Rename parse_legacy() to parse_catch_legacy()
    Resolve #75

--- tests/testthat/test-parse_legacy.R
+++ tests/testthat/test-parse_legacy.R
@@ -5,37 +5,52 @@ context("- legacy()")
 test_that("legacy object can be created", {
   expect_is(legacy(fname = "foo.xlsx",
                    year = 2020,
+                   type = "catch",
                    spcs = "maaji"),
-            "maaji")
+            "catch_maaji")
 
   expect_is(legacy(fname = "foo.xlsx",
                    year = 2020,
+                   type = "foo",
                    spcs = "bar"),
-            "bar")
+            "foo_bar")
 })
 
 test_that("throw error without args", {
   expect_error(
     expect_is(legacy(year = 2020,
+                     type = "catch",
                      spcs = "maaji"),
-              "maaji"),
+              "catch_maaji"),
     '"fname" is missing'
   )
 
   expect_error(
    expect_is(legacy(fname = "foo.xlsx",
-                    spcs = "maaji"),
-             "maaji"),
+                    type  = "catch",
+                    spcs  = "maaji"),
+             "catch_maaji"),
     '"year" is missing'
   )
+
+  expect_error(
+    expect_is(legacy(fname = "foo.xlsx",
+                     year = 2020,
+                     spcs = "maaji"),
+              "maaji"),
+    '"type" is missing'
+  )
+
 })
 
-context("- parse_legacy()")
+context("- parse_catch_legacy()")
 test_that("", {
   expect_df_with_na_coercion(
-    parse_legacy(list(fname = "excel/gyokaikyo_dummy_maaji.xlsx",
-                      spcs  = "maaji",
-                      year  = 2019))
+    parse_catch_legacy(
+      list(fname = "excel/gyokaikyo_dummy_maaji.xlsx",
+           spcs  = "maaji",
+           year  = 2019)
+    )
   )
 })
 
@@ -45,6 +60,7 @@ test_that("Excel is parsed into df", {
   expect_df_with_na_coercion(
      xl2df(fname = "excel/gyokaikyo_dummy_maaji.xlsx",
            year  = 2019,
+           type  = "catch",
            spcs  = "maaji")
   )
 })
